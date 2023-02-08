c09f14929e6cb6e1bcbe677ba07bb507a55d3abd
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2023-02-08 23:36:23 +0000

    use requested intervals

---
+++ funcs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "funcs.h"
+
+void print_interval(int L,int U){
+
+  for(int i=L; i<U; i++)
+  {
+    std::cout << i <<" ";
+  }
+
+  }
+
---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+Author: Ivy Lin
+Course: CSCI-135
+Instructor: Professor Zamansky
+Assignment: Task B
+Print all integers from the requested interva
+*/
+
+
+#include <iostream>
+#include "funcs.h"
+
+int main()
+{
+  int number;
+//print from 1-10
+  for(number =1; number<=10;number++)
+    {
+      std::cout << number<<"";
+    }
+  return 0;
+}
